8444c174c22736f5f766b96198a4bddaee64608a
Turret2075 <turret207510@gmail.com>
2025-10-06 19:13:25 -0600

    Update XDRIVE_FC - Normalized Motors.c
    Fixed formulas

--- XDRIVE_FC - Normalized Motors.c
+++ XDRIVE_FC - Normalized Motors.c
@@ -51,8 +51,8 @@ task main()
 		RawX = getJoystickValue(ChB);
 		RawY = getJoystickValue(ChA);
 		RawRX = getJoystickValue(ChC);
-		FieldcX = (RawX * cosDegrees(GyroYaw)) + (RawY * sinDegrees(GyroYaw));
-		FieldcY = (RawX * sinDegrees(GyroYaw)) - (RawY * cosDegrees(GyroYaw));
+		FieldcX = (RawX * cosDegrees(GyroYaw)) - (RawY * sinDegrees(GyroYaw));
+		FieldcY = (RawY * cosDegrees(GyroYaw)) + (RawX * sinDegrees(GyroYaw));
 		FL = (FieldcY + FieldcX + RawRX);
 		FR = (FieldcY - FieldcX - RawRX);
 		RL = (FieldcY - FieldcX + RawRX);
@@ -78,4 +78,3 @@ task main()
 		sleep(20);
 	}
 }
-
